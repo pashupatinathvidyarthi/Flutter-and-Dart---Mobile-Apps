fd24bc4bd986a180685617d1d1eb52c1cc28b082
Pashupati Nath Vidyarthi <254213285+pashupatinathvidyarthi@users.noreply.github.com>
2026-03-03 17:53:18 +0530

    Dart

--- Dart Programming Language/1.2_String.dart
+++ Dart Programming Language/1.2_String.dart
@@ -2,10 +2,17 @@ void main()
 {
   String name="Pashupati";
   String framework='Flutter';
+
+  int a=10;
+  int b=20;
+
   print(name);
   print(framework);
   print(name.length);
   print("I Love $framework");  //This is called String Interpolation
   print("The length of $framework is ${framework.length}");
   print("I Love Flutter");
+
+
+  print("The sum of $a and $b is ${a+b}");
 }
